db7267c8028106c8983d26c910dcd6ec5235b569
Max <max.krone@yahoo.com>
2021-07-20 16:33:39 -0700

    Button Fun 1.1

--- Name/ViewController.swift
+++ Name/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var labeloflabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         // Do any additional setup after loading the view.
+       
+        }
+    
+   // @IBAction func messageButtonPress(_ sender: UIButton){
+       // messageLabel1.text = "Hello"
+    
+    @IBAction func buttonStuff(_ sender: Any) {
+        labeloflabel.text = "Stop"
     }
-
 
 }
 
